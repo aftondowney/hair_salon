af065d37a1f265e710a941a57d1502ef9e7e55e3
Afton Downey <aaike001@gmail.com>
2016-02-26 15:02:48 -0800

    make changes to app.php for edit and delete functionality

--- app/app.php
+++ app/app.php
@@ -19,76 +19,76 @@
 		use Symfony\Component\HttpFoundation\Request;
 		Request::enableHttpMethodParameterOverride();
 
-		// Home page showing cuisines
+		// Home page showing stylists
 		$app->get("/", function() use ($app)
 		{
-				return $app['twig']->render('home.html.twig', array('cuisines' => Stylist::getAll(), 'form' => false));
+				return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll(), 'form' => false));
 		});
 
-		$app->post("/cuisines", function() use ($app)
+		$app->post("/stylists", function() use ($app)
 		{
-				$cuisine = new Stylist ($_POST['type']);
-				$cuisine->save();
-				return $app['twig']->render('home.html.twig', array('cuisines' => Stylist::getAll()));
+				$stylist = new Stylist ($_POST['name']);
+				$stylist->save();
+				return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
 		});
 
-		$app->get("/cuisines/{id}/edit_form", function($id) use ($app)
+		$app->get("/stylists/{id}/edit_form", function($id) use ($app)
 		{
-				$current_cuisine = Stylist::find($id);
-				$cuisines = Stylist::getAll();
-				return $app['twig']->render('home.html.twig', array('current_cuisine' => $current_cuisine, 'cuisines' => $cuisines, 'form' => true));
+				$current_stylist = Stylist::find($id);
+				$stylists = Stylist::getAll();
+				return $app['twig']->render('index.html.twig', array('current_stylist' => $current_stylist, 'stylists' => $stylists, 'form' => true));
 		});
 
-		$app->patch("/cuisines/updated", function() use ($app)
+		$app->patch("/stylists/updated", function() use ($app)
 		{
-				$cuisine_to_edit = Stylist::find($_POST['current_cuisineId']);
-				$cuisine_to_edit->update($_POST['type']);
-				return $app['twig']->render('home.html.twig', array('cuisines' => Stylist::getAll()));
+				$stylist_to_edit = Stylist::find($_POST['current_stylistId']);
+				$stylist_to_edit->update($_POST['name']);
+				return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
 		});
 
-		$app->delete("/cuisines/{id}/delete", function($id) use ($app)
+		$app->delete("/stylists/{id}/delete", function($id) use ($app)
 		{
-				$cuisine = Stylist::find($id);
-				$cuisine->delete();
-				return $app['twig']->render('home.html.twig', array('cuisines' => Stylist::getAll(), 'form' => false));
+				$stylist = Stylist::find($id);
+				$stylist->delete();
+				return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll(), 'form' => false));
 		});
 
-		// Specific cuisine pages (show restaurants)
-		$app->get("/cuisine/{id}", function($id) use ($app)
+		// Specific stylist pages (show clients)
+		$app->get("/stylist/{id}", function($id) use ($app)
 		{
-				$cuisine = Stylist::find($id);
-				return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getClients(), 'form' => false));
+				$stylist = Stylist::find($id);
+				return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients(), 'form' => false));
 		});
 
-		$app->post("/restaurants", function() use ($app)
+		$app->post("/clients", function() use ($app)
 		{
-				$restaurant = new Client($_POST['name'], $_POST['address'], $_POST['cuisine_id'], $_POST['description']);
-				$restaurant->save();
-				$cuisine = Stylist::find($_POST['cuisine_id']);
-				return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getClients()));
+				$client = new Client($_POST['name'], $_POST['phone_number'], $_POST['stylist_id']);
+				$client->save();
+				$stylist = Stylist::find($_POST['stylist_id']);
+				return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
 		});
 
-		$app->get("/restaurant/{cid}/{rid}/edit_form", function($cid, $rid) use ($app)
+		$app->get("/client/{cid}/{rid}/edit_form", function($cid, $rid) use ($app)
 		{
-				$current_restaurant = Client::find($rid);
-				$cuisine = Stylist::find($cid);
-				return $app['twig']->render('cuisine.html.twig', array('current_restaurant' => $current_restaurant, 'cuisine' => $cuisine, 'restaurants' => $cuisine->getClients(), 'form' => true));
+				$current_client = Client::find($rid);
+				$stylist = Stylist::find($cid);
+				return $app['twig']->render('stylist.html.twig', array('current_client' => $current_client, 'stylist' => $stylist, 'clients' => $stylist->getClients(), 'form' => true));
 		});
 
-		$app->patch("/restaurants/updated", function() use ($app)
+		$app->patch("/clients/updated", function() use ($app)
 		{
-				$restaurant_to_edit = Client::find($_POST['current_restaurantId']);
-				$restaurant_to_edit->update($_POST['name']);
-				$cuisine = Stylist::find($_POST['cuisine_id']);
-				return $app['twig']->render('cuisine.html.twig', array('restaurants' => $cuisine->getClients(), 'cuisine' => $cuisine));
+				$client_to_edit = Client::find($_POST['current_clientId']);
+				$client_to_edit->update($_POST['name']);
+				$stylist = Stylist::find($_POST['stylist_id']);
+				return $app['twig']->render('stylist.html.twig', array('clients' => $stylist->getClients(), 'stylist' => $stylist));
 		});
 
-		$app->delete("/restaurants/{cuisine_id}/{restaurant_id}/delete", function($cuisine_id, $restaurant_id) use ($app)
+		$app->delete("/clients/{stylist_id}/{client_id}/delete", function($stylist_id, $client_id) use ($app)
 		{
-				$restaurant = Client::find($restaurant_id);
-				$restaurant->delete();
-				$cuisine = Stylist::find($cuisine_id);
-				return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getClients()));
+				$client = Client::find($client_id);
+				$client->delete();
+				$stylist = Stylist::find($stylist_id);
+				return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
 		});
 
 		return $app;
