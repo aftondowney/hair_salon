e9f5e4188f9e4745ac6e5790d63f6e6662c6d327
Afton Downey <aaike001@gmail.com>
2016-02-26 09:05:06 -0800

    add deleteAll method to Stylist

--- src/Stylist.php
+++ src/Stylist.php
@@ -28,9 +28,37 @@
 
       function save()
       {
-        $GLOBALS['DB']->query("INSERT INTO stylists (name) VALUES ('{$this->name}')");
-        $this->id = $GLOBALS['DB']->lastInsertId();
+          $GLOBALS['DB']->query("INSERT INTO stylists (name) VALUES ('{$this->name}')");
+          $this->id = $GLOBALS['DB']->lastInsertId();
+      }
+
+      static function getAll()
+      {
+          $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+          $stylists = array();
+          foreach ($returned_stylists as $stylist)
+          {
+              $name = $stylist['name'];
+              $id = $stylist['id'];
+              $new_stylist = new Stylist($name, $id);
+              array_push($stylists, $new_stylist);
+          }
+          return $stylists;
+      }
+
+      static function deleteAll()
+      {
+          $GLOBALS['DB']->exec("DELETE FROM stylists;");
       }
     }
+
+
+
+
+
+
+
+
+
 
 ?>
