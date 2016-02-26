adfd6a4ca9b4454df1ff40c13c30afe1c318dd55
Afton Downey <aaike001@gmail.com>
2016-02-26 13:31:36 -0800

    add routes to app.php

--- app/app.php
+++ app/app.php
@@ -1,13 +1,52 @@
 <?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Stylist.php";
 
-		require_once __DIR__."/../vendor/autoload.php";
+    $app = new Silex\Application();
 
-		$app = new Silex/Application();
+    $server = 'mysql:host=localhost;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
-		$app->get("/", function() {
-			return "home";
-		});
-
-		return $app;
-
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+//get////////////
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+    //
+    // $app->get("/clients", function() use ($app) {
+    //     return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll()));
+    // });
+    $app->get("/stylists/{id}", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+//post////////////
+    $app->post("/clients", function() use ($app) {
+        $name = $_POST['name'];
+				$phone_number = $_POST['phone_number'];
+        $stylist_id = $_POST['stylist_id'];
+        $client = new Client($name, $phone_number, $id = null, $stylist_id);
+        $client->save();
+        $stylist = Stylist::find($stylist_id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+    $app->post("/stylists", function() use ($app) {
+        $stylist = new Stylist($_POST['name']);
+        $stylist->save();
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+    $app->post("/delete_stylists", function() use ($app) {
+        Stylist::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+    $app->post("/delete_clients", function() use ($app) {
+        Client::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+    return $app;
 ?>
